d81d5023b0e14ff9bbbc3412a0c68122e6fd3718
HuertaEnzoAriel <ehuerta@iinfo.unsj.edu.ar>
2026-08-21 16:37:35 -0300

    proyecto creado con un boton

--- proyecto-flutter/flutter_application_1/lib/main.dart
+++ proyecto-flutter/flutter_application_1/lib/main.dart
@@ -4,13 +4,37 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
+  
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int contador = 0;
+  void incrementarContador() {
+    setState(() {
+      contador++;
+    });
+  }
+// #AGREGUE COLUMNA CON CTRL+. PARANDOME EN EL WIDGET CENTER
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      home: Scaffold(body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('el contador tiene')),
+          Text("$contador"),
+        ],
+      ), floatingActionButton: FloatingActionButton(
+        onPressed: ()=> incrementarContador(),
+        child: Icon(Icons.abc_outlined, color:const Color.fromARGB(255, 255, 32, 3)
+      ),
+      ),
+      ),
     );
   }
 }
